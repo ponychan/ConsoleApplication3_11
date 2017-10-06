528a69d081e5107f576635cbcdcc2457e9168613
ponychan <rzsckbmx@163.com>
2017-10-06 12:04:48 +0800

    3.11.cpp
    值传递交换两个数字

--- ConsoleApplication3_11/ConsoleApplication311.cpp
+++ ConsoleApplication3_11/ConsoleApplication311.cpp
@@ -1,11 +1,22 @@
 // ConsoleApplication311.cpp: 定义控制台应用程序的入口点。
-//
+//交换两个整数的次序-值传递
 
 #include "stdafx.h"
+#include<iostream>
+using namespace std;
 
+void swap(int a, int b) {
+	int t = a;
+	a = b;
+	b = t;
+}
 
 int main()
 {
-    return 0;
+	int x = 5, y = 10;
+	cout << "x=" << x << "  y=" << y << endl;
+	swap(x, y);
+	cout << "x=" << x << "  y=" << y << endl;
+	return 0;
 }
 
